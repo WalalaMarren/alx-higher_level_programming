ea119199c33876c710d959a4a5c1996a4255817b
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-03-12 08:39:51 +0000

    reverse elements of a list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if (len(my_list)) == 1:
-        return my_list
-    list_reverse = my_list.reverse()
-    for x in list_reverse:
-        print("{:d}".format(x))
+    for item in reversed(my_list):
+        print("{:d}".format(item))
