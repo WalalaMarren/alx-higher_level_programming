ac136f5633e839c77d40ca70a82a47f2e20763d8
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-03-12 07:57:07 +0000

    Python data structures

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,4 +2,4 @@
 def element_at(my_list, idx):
     if (idx < 0 or idx not in range(len(my_list))):
         return None
-    print("Element at index {:d} is {}".format(idx, my_list[idx]))
+    print("Element at index {:d} is {:d}".format(idx, my_list[idx]))
