0a5999590064f5129ce3bd805f7a91a49e53c9fe
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-03-12 08:58:15 +0000

    true and false swap

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    new_list = []
     for i in range(len(my_list)):
         if(my_list[i] % 2 == 0):
-            my_list[i] = True
+            new_list.append(True)
         else:
-            my_list[i] = False
-    return my_list
+            new_list.append(False)
+    return new_list
