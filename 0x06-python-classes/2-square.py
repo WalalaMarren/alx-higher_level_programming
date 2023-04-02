d9d0085757e0b98984900c1e32058833df0cbad3
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-04-02 10:17:50 +0000

    instantiation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,10 +4,14 @@
 
 class Square:
     """Represent a square."""
-    try:
-        def __init__(self, size=0):
-            self.__size = size
-    except TypeError:
-        print("size must be an integer")
-    except ValueError:
-        print("size must be >= 0")
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
