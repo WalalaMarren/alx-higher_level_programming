2373d9de34b436a632651aa0bca41df3877071ab
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-04-02 17:07:41 +0000

    using getters and setters

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python3
+""" module contains: class Square """
 
-"""Creating a new class of Square name"""
+
 class Square:
-    """defining the class square"""
+    """
+        Square: defines a square.
+        Attributes:
+            size (int): size of square
+        Method:
+            __init__: Initializes size for all instances
+    """
+
     def __init__(self, size=0):
-        self.__size = size
+        """ Initialization of attributes for instances
+            Args:
+                size (int): size of the square.
+        """
+        self.size = size
+
 
     @property
     def size(self):
-        """getting the property of size"""
-        return self.__size
-
+        """ getter function to for private attribute size"""
+        return (self.__size)
+    
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
-        """public method to calculate the area"""
-        return self.__size ** 2
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
