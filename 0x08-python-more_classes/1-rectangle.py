d1b75fe44b7f97cb734c4d52b998e9e7799dacde
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-04-03 10:26:25 +0000

    setting up class property

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,6 +4,7 @@
 
 class Rectangle:
     """defining the class rectangle"""
+
     def __init__(self, width=0, height=0):
          """Initialize a new Rectangle.
         Args:
@@ -24,6 +25,7 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     @property
     def height(self):
         """This is a method that retrives the width value."""
