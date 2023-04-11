e4d1831e9dc650b33b4f5d335326e509ad22878d
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-04-11 15:14:11 +0000

    Inheritance implementation

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Defining a new subclass Rectangle"""
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
-    """This is a new class Rectangle which inherits property and methods of BaseGeometry"""
+    """class Rectangle which inherits property and methods of BaseGeometry"""
 
     def __init__(self, width, height):
         """This is a contructor to initialize new rectangle
