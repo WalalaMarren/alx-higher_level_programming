669af7d00ca1777ef7a5e6f009d9f3ae1365f032
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-04-11 09:48:47 +0000

    print list in ascending order

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""New Class"""
+
 class MyList(list):
     """Defining class MyList"""
     def print_sorted(self):
-        print(list.sort())
+        """public class method to sort the list"""
+        print(self.sort())
