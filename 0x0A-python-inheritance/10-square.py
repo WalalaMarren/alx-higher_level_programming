ad8121ab5b453f432cbb929972e2131ab6889bd5
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-04-11 15:41:28 +0000

    class square that inherits from rect

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defining a new subclass Rectangle"""
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    """class Square which inherits property and methods of Rectangle"""
+
+    def __init__(self, size):
+        """This is a contructor to initialize new rectangle
+        Args:
+            size(int): Parameter for the width and height private
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__size * self.__size
