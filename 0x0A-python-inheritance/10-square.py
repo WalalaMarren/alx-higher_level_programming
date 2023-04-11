61e719773c3c1db2d453eb09edd110280e977195
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-04-11 16:49:03 +0000

    class square that inherits from rect

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -12,7 +12,7 @@ class Square(Rectangle):
             size(int): Parameter for the width and height private
         """
         self.integer_validator("size", size)
-        self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         """Return the area of the rectangle."""
