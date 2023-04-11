5698f750dde5978c3dfb2b3961511f44d9f795eb
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-04-11 11:17:09 +0000

    empty method that raises exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class definition of BaseGeometry"""
+
+
+class BaseGeometry:
+    """The class above is an empty class BaseGeometry to be build on"""
+    def area(self):
+        """this is a public method with no args"""
+        raise Exception("area() is not implemented")
