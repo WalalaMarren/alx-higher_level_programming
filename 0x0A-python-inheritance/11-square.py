a1238e07b57cb884e6a0aec264b581dabb999d57
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-04-11 17:00:41 +0000

    str(), print() and new method

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defining a new subclass Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Rectangle which inherits property and methods of BaseGeometry"""
+
+    def __init__(self, size):
+        """This is a contructor to initialize new rectangle
+        Args:
+            size(int): Parameter for the width and height private
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+    
+    def area(self):
+        """This is a method to return the area of the square"""
+        return self.__size ** 2
