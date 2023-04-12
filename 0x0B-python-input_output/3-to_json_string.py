299e53c2da2cd946dc6fb5c3347ab99456760fc8
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-04-12 11:26:47 +0000

    JSON string representation

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+"""Introducing the JSON file serialization"""
+
+
+def to_json_string(my_obj):
+    """Function to return the JSON format of my_obj"""
+    return json.dumps(my_obj)
