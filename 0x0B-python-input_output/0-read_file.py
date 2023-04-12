c2db93809072ca7a38c6b7fce667f9ed855de6cd
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-04-12 10:27:03 +0000

    reads a text file and prints line

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Defining a new function"""
+
+
 def read_file(filename=""):
-    with open(filename, encoding="utf-8") as f:
+    """This is a function to read file content"""
+    with open(filename, "r", encoding="utf-8") as f:
         for line in f:
             print(line, end='')
