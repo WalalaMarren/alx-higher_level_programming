c437790e51a2c94b0196c1f5917467d8144ba945
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-04-12 13:21:57 +0000

    returns the dict rep of a JSON obj

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,14 +1,7 @@
+#!/usr/bin/python3
+"""Defines a Python class-to-JSON function."""
+
+
 def class_to_json(obj):
-    """
-    Returns a dictionary description with a simple data structure for JSON serialization of an object
-    """
-    if hasattr(obj, '__dict__'):
-        return obj.__dict__
-    elif isinstance(obj, (list, tuple)):
-        return [class_to_json(item) for item in obj]
-    elif isinstance(obj, dict):
-        return {key: class_to_json(value) for key, value in obj.items()}
-    elif isinstance(obj, (str, int, bool, type(None))):
-        return obj
-    else:
-        raise TypeError("Object of type '{}' is not JSON serializable".format(type(obj).__name__))
+    """Return the dictionary represntation of a simple data structure."""
+    return obj.__dict__
