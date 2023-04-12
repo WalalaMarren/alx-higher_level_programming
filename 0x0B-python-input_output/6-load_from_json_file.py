a32f70e804287a245f42327954ecc9f8ec016bac
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-04-12 12:26:39 +0000

    loading an object from JSON file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def load_from_json_file(filename):
     """loading a JSON file into an object"""
     with open(filename, encoding="utf-8") as f:
-        json.load(filename)
+        json.load(f)
