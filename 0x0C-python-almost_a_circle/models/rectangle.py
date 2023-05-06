c9ccc9fa4e0d8f792b22b0410e5ce5f42c832ecb
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-05-06 10:25:09 +0000

    defining a rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""definition of a new class Rectangle"""
+
+
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Definind a new class that inherits from the base
+        Args:
+            width(int): definition of width
+            height(int): definition of height
+            x(int): extra attr
+            y(int): extra attr
+            id(int): id value
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+    @property
+    def width(self):
+        """get/set the width of a rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        """get/set the height of a rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height == value
+
+    @property
+    def x(self):
+        """get/set extra value for the rectangle"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        """get/set extra value for the rectangle"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
