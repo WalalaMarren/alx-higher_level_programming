22f1b83776f2fccb95b5692ce4c76b659aa67cad
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-05-16 16:59:37 +0000

    remove database

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,3 @@
---deletes the database hbtn_0c_0 in my MySQL server
-DROP DATABASE IF EXISTS hbtn_0c_0;
+-- deletes the database hbtn_0c_0 in mysql server
+-- script does not fail if database doesn't exist
+DROP DATABASE IF EXISTS hbtn_0c_0
