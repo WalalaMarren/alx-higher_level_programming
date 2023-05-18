cced8b931e929fe43295771b67f5711f4d3538ae
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-05-18 15:25:37 +0000

    show user privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
--- showing user privilleges
-SHOW GRANTS FOR 'user_0d_1' , 'user_0d_2' @'localhost';
+-- Script that lists all privileges of the MySQL users
+-- Query to list all privileges (GRANT) of the MySQL users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
