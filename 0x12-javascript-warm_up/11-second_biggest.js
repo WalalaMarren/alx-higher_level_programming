f330c48830351e440cfb2187678490658ef517e3
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-06-12 16:09:41 +0000

    find the largest int

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-const myArgs = process.argv.slice(2);
-if (!myArgs[0] || !myArgs[2]) {
-  console.log(0);
-} else {
-  const args = myArgs.map(Number);
-  const firstMax = Math.max.apply(null, args);
-  args.splice(args.indexOf(firstMax), 1);
-  console.log(Math.max.apply(null, args));
-}
+let i = 0;
+const array = process.argv.slice(2, process.argv.length);
+if (array.length > 1) { array.sort(function (a, b) { return b - a; }); i = array[1]; }
+console.log(i);
